65fc19487741a6ed87ec2fc9a5595025476aa416
Maurizioore <mauriziorenzi68@gmail.com>
2022-11-01 17:36:45 +0100

    altra prova

---
+++ src/uniroma2/eu/Main.java
@@ -0,0 +1,13 @@
+package uniroma2.eu;
+
+import Kom.Altri;
+import Kom.Saluti;
+
+public class Main {
+    public static void main(String[] args) {
+        Saluti neo=new Saluti();
+        neo.fine();
+        Altri ini=new Altri();
+        ini.inizio();
+    }
+}
