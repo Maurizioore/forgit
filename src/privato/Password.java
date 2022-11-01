bfbb6b31670c9d898d5e8cc868ea04c8ad753715
Maurizioore <116298592+Maurizioore@users.noreply.github.com>
2022-11-01 17:53:57 +0100

    Update Password.java

--- src/privato/Password.java
+++ src/privato/Password.java
@@ -3,7 +3,7 @@ package privato;
 import java.util.Scanner;
 
 public class Password {
-    private String pass="spumeggianteee";
+    private String pass="piu_spumeggiante";
     private void showpass(){
         System.out.println("si la password e' corretta");
     }
